6aed4bdde7b16fdb3f03b3a3f54551b2ea59a4ff
wenchuanlu <130435110+wenchuanlu@users.noreply.github.com>
2024-06-27 09:59:18 +0900

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,23 @@
+
+void foo()
+{
+	int *p = 0;
+	
+	*p = 0;
+}
+
+void foo11()
+{
+	int *p;
+	
+	*p = 0;
+}
+
+void foo2()
+{
+	short p = 0;
+	unsigned q = 0;
+	
+	p = q;
+}
+
---
+++ test2.c
@@ -0,0 +1,23 @@
+
+void bar()
+{
+	int *p, i;
+	
+	*p = 0;
+
+
+	for (i = 0; i < 5; i++)
+	{
+
+	}
+}
+
+static void bar2()
+{
+	int i, j;
+	int *p;
+	if (0 == p)
+	{
+		return;
+	}
+}
